4df26ec4fa7796982df3ba1dbb1f68f56e1deeb2
michelle422 <chelo.pablo@gmail.com>
2022-10-24 10:50:22 +0200

    Search products by keyword

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -4,7 +4,7 @@ import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
-  selector: 'app-product-list', 
+  selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css']
 })
@@ -12,9 +12,10 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId: number = 1;
+  searchMode: boolean = false;
 
-  constructor(private productService: ProductService, 
-              private route: ActivatedRoute) { }
+  constructor(private productService: ProductService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(() => {
@@ -23,6 +24,30 @@ export class ProductListComponent implements OnInit {
   }
 
   listProducts() {
+
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    } else {
+      this.handleListProducts();
+    }
+
+  }
+
+  handleSearchProducts() {
+    
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    // now search for the products using keyword
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
+
+  handleListProducts() {
 
     // check if "id" parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
@@ -41,6 +66,7 @@ export class ProductListComponent implements OnInit {
         this.products = data;
       }
     )
+
   }
 
 }
